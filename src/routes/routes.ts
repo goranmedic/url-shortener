887f396c070346177c30ac92ab6aa7d493938b36
gmedic <gmedic@neogov.net>
2021-08-09 10:23:38 +0200

    added actual routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,24 +1,54 @@
 import { Express, NextFunction, Request, Response } from "express";
+import iocContainer from "../ioc/inversify.config";
+import { TYPES } from "../ioc/types";
+import { IUrlMapperService } from "../services/interfaces";
 
 export const routes = (app: Express) => {
   app.get(
     "/",
     async (request: Request, response: Response, next: NextFunction) => {
-      return response.json("GET OK");
+      try {
+        const service = iocContainer.get<IUrlMapperService>(
+          TYPES.IUrlMapperService
+        );
+        const items = service.getList();
+
+        return response.json(items);
+      } catch (error) {
+        next(error);
+      }
     }
   );
 
   app.get(
     "/:short_url",
     async (request: Request, response: Response, next: NextFunction) => {
-      return response.json("GET BY URL OK");
+      try {
+        const service = iocContainer.get<IUrlMapperService>(
+          TYPES.IUrlMapperService
+        );
+        const item = service.getByShortUrl(request.params.short_url);
+
+        return response.redirect(item.url);
+      } catch (error) {
+        next(error);
+      }
     }
   );
 
   app.post(
     "/",
     async (request: Request, response: Response, next: NextFunction) => {
-      return response.json("POST OK");
+      try {
+        const service = iocContainer.get<IUrlMapperService>(
+          TYPES.IUrlMapperService
+        );
+        const item = service.shorten(request.body.url);
+
+        response.json({ url: item.url, short_url: item.shortUrl });
+      } catch (error) {
+        next(error);
+      }
     }
   );
 };
